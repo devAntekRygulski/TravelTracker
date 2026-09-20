62bd0222ac2ad854c1b3176474bd5af3778f80e1
devAntekRygulski <amrygulski@dons.usfca.edu>
2026-09-20 23:07:28 +0200

    Add country names

--- src/data/countryNameImages.ts
+++ src/data/countryNameImages.ts
@@ -6,6 +6,15 @@ const COUNTRY_NAME_IMAGES: Record<string, string> = {
   '840': '/country_names/usa.png',
   '124': '/country_names/canada.png',
   '484': '/country_names/mexico.png',
+  '084': '/country_names/belize.png',
+  '250': '/country_names/france.png',
+  '304': '/country_names/greenland.png',
+  '320': '/country_names/guatemala.png',
+  '340': '/country_names/honduras.png',
+  '352': '/country_names/iceland.png',
+  '620': '/country_names/portugal.png',
+  '724': '/country_names/spain.png',
+  '826': '/country_names/unitedkingdom.png',
 };
 
 export function getCountryNameImageSrc(countryId: string): string | null {
